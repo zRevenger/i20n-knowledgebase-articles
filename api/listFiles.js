9e27c788f091398f0f5c68a3b7157606047505be
zRevenger <56997948+zRevenger@users.noreply.github.com>
2025-09-15 19:34:31 +0200

    Allowing call origins

--- api/listFiles.js
+++ api/listFiles.js
@@ -1,4 +1,7 @@
 export default async function handler(req, res) {
+  
+    res.setHeader("Access-Control-Allow-Origin", "*");
+
   const { type } = req.query;
 
   // In base al tipo cambio la cartella
