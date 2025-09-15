83e9bd4e92b3c5c96debf50b4eb70b9a8fbccaff
zRevenger <56997948+zRevenger@users.noreply.github.com>
2025-09-15 23:10:10 +0200

    Update saveArticle.js

--- api/saveArticle.js
+++ api/saveArticle.js
@@ -8,6 +8,11 @@ export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+  
   if (req.method !== "POST") return res.status(405).end();
 
   const { path, frontmatter, content, message } = req.body;
